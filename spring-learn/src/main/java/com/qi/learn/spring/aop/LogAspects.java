01cf14aeacb53dbd5ac9c11ac472801f3fafb361
zhangleqi <463456910@qq.com>
2019-06-30 22:42:07 +0800

    增加切面

--- spring-learn/src/main/java/com/qi/learn/spring/aop/LogAspects.java
+++ spring-learn/src/main/java/com/qi/learn/spring/aop/LogAspects.java
@@ -4,8 +4,10 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class LogAspects {
 	@Pointcut("execution( * com.qi.learn.spring.service.*.*(..))")
 	private void pointCut() {
